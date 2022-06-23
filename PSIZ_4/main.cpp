a52ba41a0c8669b8e8c18f5ae615805ce03ff10b
KrzysiekPietrzak <56595451+KrzysiekPietrzak@users.noreply.github.com>
2022-06-23 07:02:37 +0200

    commit 3

--- PSIZ_4/main.cpp
+++ PSIZ_4/main.cpp
@@ -1,4 +1,4 @@
-﻿#include "pch.h"
+#include "pch.h"
 
 struct berResults
 {
@@ -14,11 +14,11 @@ void createFile1(const std::string name, const int count, const char value);
 berResults calculateBer(std::string fpath1, std::string fpath2);
 void printResult(berResults results);
 
-int main(int argc, char* argv[])
+int main(int argc, char * argv[])
 {
     std::string fpath1; //path to file 1
     std::string fpath2; //path to file 2
-    berResults results;
+    berResults results; 
 
     openLog("log.log"); //create log.log file for logging purposes
     if (argc != 3) //check the number of arguments
@@ -29,7 +29,7 @@ int main(int argc, char* argv[])
         createFile1("test1_file1.bin", 100, 0xFF); //1111 1111
         createFile1("test1_file2.bin", 100, 0xFE); //1111 1110
         //test 2
-
+        
         /*
             missing function to change 10 bits in the file
         */
@@ -38,7 +38,7 @@ int main(int argc, char* argv[])
         //createFile1("test3_file1.bin",400000000,0x55); 
         //createFile1("test3_file2.bin",400000000,0x50);
         saveLog("Test files are prepared");
-        saveLog("Re-run with correct arguments ie: ./PSIZ_4.exe test1_file1.bin test1_file2.bin");
+        saveLog("Re-run with correct arguments ie: ./task_iv_ber.exe test1_file1.bin test1_file2.bin");
     }
     else //arguments are passed correctly
     {
@@ -72,7 +72,7 @@ void createFile1(const std::string name, const int count, const char value)
     f.open(name.c_str(), std::ios::binary | std::ios::out);
     for (int i = 0; i < count; i++)
     {
-        f.write((char*)&value, 1);
+        f.write((char*)&value,1);
     }
     f.close();
 }
@@ -106,7 +106,7 @@ berResults calculateBer(std::string fpath1, std::string fpath2)
     }
 
     results.ber = (float)results.err / results.tot; // calculate ber
-    results.t2 = clock();
+    results.t2 = clock(); 
     saveLog("BER calculations are done");
     return results; //return structure with all results
 }
